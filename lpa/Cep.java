5f1591d6c1e6941ada57a82579dee09af1b1a16c
Tatiane Ximenes <ximenes_taty@hotmail.com>
2025-11-09 11:36:28 -0300

    definição da classe Cep

---
+++ lpa/Cep.java
@@ -0,0 +1,21 @@
+package app.modelos.prescritivos;
+
+public class Cep {
+
+	// Attributes
+	private String numero;
+	private String nome;
+	
+	
+	//constructor
+	public Cep(String numero, String nome) {
+		this.numero = numero;
+		this.nome = nome;
+	}
+	
+	//methods
+	public String pesquisarLogradouro() {
+		return nome;
+	}
+	
+}
